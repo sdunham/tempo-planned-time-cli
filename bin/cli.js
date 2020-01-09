9272f6d5b626922d1f4f3c4aa759e894d4804c66
Scott Dunham <sdunham@sitecrafting.com>
2020-01-09 13:05:53 -0800

    Updated yargs config to show help screen with no error message when no or unrecognized command is provided, doc updates

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,6 @@
 const yargonaut = require('yargonaut');
 const yargs = require('yargs');
 
-// TODO: https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
-// TODO: Docs
-// TODO: Installation options/instructions
-
 // Set up some basic output styles using yargonaut 
 yargonaut
   .style('yellow')
@@ -18,11 +14,12 @@ yargs
   .scriptName('tempo-planned-time')
   .usage('$0 <cmd> [args]')
   .commandDir('commands')
-  .demandCommand()
   .example('$0 config --account foo --token bar','Sets your accountId to "foo" and accessToken to "bar"')
   .example('$0 config --url https://mycompany.atlassian.net','Sets your jiraBaseUrl to "https://mycompany.atlassian.net"')
   .example('$0 get','Gets planned time for the current day')
   .example('$0 get --from 2020-01-10 --to 2020-01-12','Gets planned time between 2020-01-10 and 2020-01-12')
   .wrap(Math.min(130, yargs.terminalWidth()))
+  .demandCommand(1, '')
+  .strict()
   .help()
   .argv
